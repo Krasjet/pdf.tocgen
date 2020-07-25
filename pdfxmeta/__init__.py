5f7acd4882725dabb341389308352f505b782b5e
krasjet <nil@krj.st>
2020-07-25 06:07:16 -0700

    add tests for xmeta

---
+++ pdfxmeta/__init__.py
@@ -0,0 +1,1 @@
+from .pdfxmeta import extract_meta
---
+++ tests/test_xmeta.py
@@ -0,0 +1,47 @@
+import unittest
+from pdfxmeta import extract_meta
+import fitz
+
+
+class ExtractMetaTest(unittest.TestCase):
+    def test_extract_meta(self):
+        doc = fitz.open('./tests/files/level2.pdf')
+        meta = extract_meta(doc, "Section One", 1)
+        self.assertEqual(len(meta), 1)
+        self.assertIn('font', meta[0])
+        self.assertIn('CMBX12', meta[0]['font'])
+
+    def test_extract_meta_ign_case(self):
+        doc = fitz.open('./tests/files/level2.pdf')
+        #
+        meta = extract_meta(doc, "section one", 1, True)
+        self.assertEqual(len(meta), 1, "ignore case should match lowercase")
+        self.assertIn('font', meta[0])
+        self.assertIn('CMBX12', meta[0]['font'])
+
+        meta = extract_meta(doc, "sEcTIoN OnE", 1, True)
+        self.assertEqual(len(meta), 1, "ignore case should match mixed case")
+        self.assertIn('font', meta[0])
+        self.assertIn('CMBX12', meta[0]['font'])
+
+        meta = extract_meta(doc, "section one", 1, False)
+        self.assertEqual(len(meta), 0,
+                         "without ignore case, lowercase shouldn't match anything")
+
+    def test_extract_meta_multiple(self):
+        doc = fitz.open('./tests/files/level2.pdf')
+
+        meta = extract_meta(doc, "Section", 1)
+        self.assertEqual(len(meta), 2, "should match 2 instances")
+
+        self.assertIn('font', meta[0])
+        self.assertIn('CMBX12', meta[0]['font'])
+
+        self.assertIn('font', meta[1])
+        self.assertIn('CMBX12', meta[1]['font'])
+
+    def test_extract_meta_none(self):
+        doc = fitz.open('./tests/files/level2.pdf')
+
+        meta = extract_meta(doc, "Sectoin", 1)
+        self.assertEqual(len(meta), 0, "should match none")
