501f93b963b7918ce61a39a2d09f0bdf0140864b
krasjet <nil@krj.st>
2020-07-27 09:55:28 -0700

    add tests for fitzutils

--- spec/fitzutils_spec.py
+++ spec/fitzutils_spec.py
@@ -1,7 +1,7 @@
 import os
 
 from mamba import description, it
-from fitzutils import open_pdf
+from fitzutils import *
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
 
@@ -26,3 +26,37 @@ with description("open_pdf:") as self:
             raise err
         except:
             pass
+
+with description("ToCEntry") as self:
+    with it("matches fitz's representation"):
+        fitz_entry = [1, "title", 2]
+        fitz_entry2 = [1, "title", 2, 100.0]
+
+        toc_entry = ToCEntry(level=1, title="title", pagenum=2)
+        toc_entry2 = ToCEntry(level=1, title="title", pagenum=2, vpos=100.0)
+
+        assert toc_entry.to_fitz_entry() == fitz_entry
+        assert toc_entry2.to_fitz_entry() == fitz_entry2
+
+        assert ToCEntry(*fitz_entry) == toc_entry
+        assert ToCEntry(*fitz_entry2) == toc_entry2
+
+    with it("is sorted correctly"):
+        entries = [
+            ToCEntry(level=1, title="title4", pagenum=2, vpos=150.0),
+            ToCEntry(level=1, title="title3", pagenum=2, vpos=90.0),
+            ToCEntry(level=1, title="title5", pagenum=3, vpos=0.0),
+            ToCEntry(level=1, title="title2", pagenum=1, vpos=150.0),
+            ToCEntry(level=1, title="title1", pagenum=1, vpos=100.0),
+            ToCEntry(level=1, title="title6", pagenum=5, vpos=200.0)
+        ]
+
+        expected = [
+            ToCEntry(level=1, title="title1", pagenum=1, vpos=100.0),
+            ToCEntry(level=1, title="title2", pagenum=1, vpos=150.0),
+            ToCEntry(level=1, title="title3", pagenum=2, vpos=90.0),
+            ToCEntry(level=1, title="title4", pagenum=2, vpos=150.0),
+            ToCEntry(level=1, title="title5", pagenum=3, vpos=0.0),
+            ToCEntry(level=1, title="title6", pagenum=5, vpos=200.0)
+        ]
+        assert sorted(entries, key=ToCEntry.key) == expected
